3a40c01aabde5ec33324f41f82dcf49506c469c5
Zack Spate <zspate@asu.edu>
2022-03-19 20:31:57 -0600

    Saved work @ sbux

---
+++ Snippet-Testing/Snippysnip.java
@@ -0,0 +1,9 @@
+public class Snippysnip{
+    public static void main(String[] args) {
+        
+    String str = "Hello";
+    int n = str.length();
+        String mystery = str.substring(0, 1)
+        + str.substring(beginIndex)
+    }
+}
