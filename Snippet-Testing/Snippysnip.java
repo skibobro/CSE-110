e38445925bd3d891e91ef836975779ebd7b61918
Zack Spate <zspate@asu.edu>
2022-03-21 13:31:00 -0600

    updating and cleaning up repo

--- Snippet-Testing/Snippysnip.java
+++ Snippet-Testing/Snippysnip.java
@@ -1,9 +1,6 @@
 public class Snippysnip{
     public static void main(String[] args) {
         
-    String str = "Hello";
-    int n = str.length();
-        String mystery = str.substring(0, 1)
-        + str.substring(beginIndex)
+
     }
 }
